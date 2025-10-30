2e346e53ee859a68da1ac6de7fd41a2bdb9c0a56
ThomsonW04 <tyclonie@gmail.com>
2025-10-30 23:30:27 +0000

    invoicer details now built

--- PayPy/PayPyInvoicer.py
+++ PayPy/PayPyInvoicer.py
@@ -16,20 +16,34 @@ class PayPyInvoicer:
 
     async def create_invoice(self, 
                              currency_code: str, 
+                             invoicer_given_names: str,
+                             invoicer_surname: str,
+                             invoicer_email_address: str,
                              invoice_number: str = None, 
                              reference: str = None, 
                              invoice_date: str = None, 
                              note: str = None, 
                              term: str = None, 
                              memo: str = None,
-                             payment_term: dict = None # Expects dict of term_type and due_date
+                             payment_term: dict = None, # Expects dict of term_type and due_date
+                             invoicer_address_line_one: str = None,
+                             invoicer_address_line_two: str = None,
+                             invoicer_admin_area_2: str = None,
+                             invoicer_admin_area_1: str = None,
+                             invoicer_postal_code: str = None,
+                             invoicer_country_country_code: str = None,
+                             invoicer_phones: list = None, # Expects list of dicts of country_code (int), national_number (int), phone_type (string)
+                             invoicer_website: str = None,
+                             invoicer_tax_id: str = None,
+                             invoicer_logo_url: str = None,
+                             invoicer_additional_notes: str = None,
                              ):
         
         # Initialise the data dictionary
         data = {}
 
         # Begin building the detail chunk of the invoice
-        data['detail'] = {
+        data["detail"] = {
             "invoice_number": invoice_number if invoice_number is not None else await self.__get_next_invoice_number(),
             "currency_code": currency_code
         }
@@ -46,18 +60,35 @@ class PayPyInvoicer:
         if payment_term is not None:
             data['detail']['payment_term'] = payment_term
 
-"""
-"detail": {
-"invoice_number": "#123",
-"reference": "deal-ref",
-"invoice_date": "2018-11-12",
-"currency_code": "USD",
-"note": "Thank you for your business.",
-"term": "No refunds after 30 days.",
-"memo": "This is a long contract",
-"payment_term": {
-"term_type": "NET_10",
-"due_date": "2018-11-22"
-}
-},
-"""
+        # Begin building invoicer chunk of the invoice
+
+        data["invoicer"] = {
+            "name": {
+                "given_name": invoicer_given_names,
+                "surname": invoicer_surname
+            },
+            "email_address": invoicer_email_address
+        }
+
+        if invoicer_address_line_one is not None:
+            data["invoicer"]["address"]["address_line_1"] = invoicer_address_line_one
+        if invoicer_address_line_two is not None:
+            data["invoicer"]["address"]["address_line_2"] = invoicer_address_line_two
+        if invoicer_admin_area_1 is not None:
+            data["invoicer"]["address"]["admin_area_1"] = invoicer_admin_area_1
+        if invoicer_admin_area_2 is not None:
+            data["invoicer"]["address"]["admin_area_2"] = invoicer_admin_area_2
+        if invoicer_postal_code is not None:
+            data["invoicer"]["address"]["postal_code"] = invoicer_postal_code
+        if invoicer_country_country_code is not None:
+            data["invoicer"]["address"]["country_code"] = invoicer_country_country_code
+        if invoicer_phones is not None:
+            data["invoicer"]["address"]["phones"] = invoicer_phones
+        if invoicer_website is not None:
+            data["invoicer"]["address"]["website"] = invoicer_website
+        if invoicer_tax_id is not None:
+            data["invoicer"]["address"]["tax_id"] = invoicer_tax_id
+        if invoicer_logo_url is not None:
+            data["invoicer"]["address"]["logo_url"] = invoicer_logo_url
+        if invoicer_additional_notes is not None:
+            data["invoicer"]["address"]["additional_notes"] = invoicer_additional_notes
